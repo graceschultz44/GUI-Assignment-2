1906811757ac2ea33a538df037f2a1f82b6a03dc
Grace Schultz <153847221+graceschultz44@users.noreply.github.com>
2024-03-05 17:02:01 -0600

    Create ProductReview.js

---
+++ ProductReview.js
@@ -0,0 +1,8 @@
+export class ProductReview {
+    constructor(userName, rating, comment, date) {
+        this.userName = userName;
+        this.rating = rating;
+        this.comment = comment;
+        this.date = date;
+    }
+}
