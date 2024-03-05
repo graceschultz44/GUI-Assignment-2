146ac2015fc47ddac0280187b7f4599b0978a5b2
Grace Schultz <153847221+graceschultz44@users.noreply.github.com>
2024-03-05 17:01:43 -0600

    Create Product.js

---
+++ Product.js
@@ -0,0 +1,10 @@
+export class Product {
+    constructor(id, name, description, price, imageUrl, reviews) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.reviews = reviews;
+    }
+}
