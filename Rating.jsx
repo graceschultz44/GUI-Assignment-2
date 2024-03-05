1342feb540aef61d65d175cfb5f7cdbce7419919
Grace Schultz <153847221+graceschultz44@users.noreply.github.com>
2024-03-05 16:57:02 -0600

    Create Rating.jsx

---
+++ Rating.jsx
@@ -0,0 +1,18 @@
+import "./rating.css"
+
+export const Rating = (value) => {
+    return (
+        <span className = "stars">
+            {
+                [1,2,3,4,5].map(x => (
+                    <i key = { x }
+                        className = {(
+                                x > value.value ? "empty-star" : "full-star"
+                            )}
+                    >
+                    </i>
+                ))
+            }
+        </span>
+    )
+}
